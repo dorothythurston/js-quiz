c9a4b04ca1a8336e5893a2babe9e230ba777b1a9
Dorothy Thurston <dorothy.thur@gmail.com>
2015-03-05 17:44:51 -0500

    Add text remove and replace. Add basic question objects.

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 $(document).ready(function() {
+var questionOne = {question:"Who wore glass slippers?",  answerOne:["Belle", 0], answerTwo:["Cinderella", 1], answerThree:["Pochahontas",0]},
+    questionTwo = {question:"Who lived in the Ocean?", answerOne:["Ariel", 1], answerTwo:["Merida",0], answerThree:["Mulan",0]},
+    questionThree = {question:"Who was from New Orleans?", answerOne:["Tiana", 1], answerTwo:["Elsa", 0], answerThree:["Rapunzel", 0]},
+    questionFour = {question:"Who had a pet tiger?", answerOne:["Snow White", 0], answerTwo:["Jasmine", 1], answerThree:["Aurora", 0]},
+    questions = [questionOne, questionTwo, questionThree, questionFour];
+var i = 0;
+var counter = [];
+
+
   $('.answer').click(function() {
-    $(this).toggleClass('selected')
+    $(this).toggleClass('selected');
+    if (i === questions.length-1) {
+      $('#quiz').slideUp( 1200 );
+      $('#status').slideUp( 1200 );
+      $('.key-container').fadeIn( 1200 );
+    }
+    else {
+      i+=1;
+      $('p', '#prompt').text((questions[i]).question);
+      $('#choice-one').text((questions[i]).answerOne[0]);
+      $('#choice-two').text((questions[i]).answerTwo[0]);
+      $('#choice-three').text((questions[i]).answerThree[0]);
+      $(this).toggleClass('selected');
+    }
   });
 
   $('.key-container').click(function() {
-    $(this).fadeOut( 400 );
-    $('#quiz').fadeIn( 500 );
-    $('#status').fadeIn( 500 );
+    $(this).fadeOut( 500 );
+    $('p', '#prompt').text(questionOne.question);
+    $('#choice-one').text(questionOne.answerOne[0]);
+    $('#choice-two').text(questionOne.answerTwo[0]);
+    $('#choice-three').text(questionOne.answerThree[0]);
+    $('#quiz').slideDown( 1200 );
+    $('#status').slideDown( 1200 );
   });
-
-
-/*
-
-var question1= {prompt: "Who is from New Orleans?", answer1:["Tiana", true], answer2:["Ariel",false], answer3:["Belle", false]};
-var question2= {prompt: "text", answer1:["text", true], answer2:["text",false], answer3:["text", false]};
-var question3= {prompt: "text", answer1:["text", true], answer2:["text",false], answer3:["text", false]};
-var question4= {prompt: "text", answer1:["text", true], answer2:["text",false], answer3:["text", false]};
-
-var correct = 0;
-var totalQuestions = questions.length;
-
-var questions = [question1, question2, question3, question4]
-*/
-
 });
