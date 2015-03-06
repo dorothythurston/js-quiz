e802268d9dcca1afe78d83c34271fd4c384a13fa
dorothy <dorothy.thur@gmail.com>
2015-03-05 20:33:22 -0500

    Add score counting and results.

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
 $(document).ready(function() {
-var questionOne = {question:"Who wore glass slippers?",  answerOne:["Belle", 0], answerTwo:["Cinderella", 1], answerThree:["Pochahontas",0]},
-    questionTwo = {question:"Who lived in the Ocean?", answerOne:["Ariel", 1], answerTwo:["Merida",0], answerThree:["Mulan",0]},
-    questionThree = {question:"Who was from New Orleans?", answerOne:["Tiana", 1], answerTwo:["Elsa", 0], answerThree:["Rapunzel", 0]},
-    questionFour = {question:"Who had a pet tiger?", answerOne:["Snow White", 0], answerTwo:["Jasmine", 1], answerThree:["Aurora", 0]},
+var questionOne = {question:"Who wore glass slippers?", answer:"Cinderella", wrongOne: "Belle", wrongTwo: "Pochahontas"},
+    questionTwo = {question:"Who lived in the Ocean?", answer:"Ariel", wrongOne:"Merida", wrongTwo:"Mulan"},
+    questionThree = {question:"Who was from New Orleans?", answer:"Tiana", wrongOne:"Elsa", wrongTwo:"Rapunzel"},
+    questionFour = {question:"Who had a pet tiger?", answer:"Jasmine", wrongOne:"Snow White", wrongTwo:"Aurora"},
     questions = [questionOne, questionTwo, questionThree, questionFour];
 var i = 0;
-var counter = [];
+var score = 0;
+
 
 
   $('.answer').click(function() {
     $(this).toggleClass('selected');
     if (i === questions.length-1) {
-      $('#quiz').slideUp( 1200 );
-      $('#status').slideUp( 1200 );
+      $('.answer').slideUp( 400 );
+      $('#prompt').fadeOut( 800 );
+      $('#status').fadeOut( 800 );
       $('.key-container').fadeIn( 1200 );
+      $('#results').fadeIn( 1200 );
+      $('#results').text("You got " + score + " correct.");
     }
     else {
-      i+=1;
+      i++;
       $('p', '#prompt').text((questions[i]).question);
-      $('#choice-one').text((questions[i]).answerOne[0]);
-      $('#choice-two').text((questions[i]).answerTwo[0]);
-      $('#choice-three').text((questions[i]).answerThree[0]);
+      $('#choice-one').text((questions[i]).answer);
+      $('#choice-two').text((questions[i]).wrongOne);
+      $('#choice-three').text((questions[i]).wrongTwo);
       $(this).toggleClass('selected');
+      if ($(this).text().trim() === questions[i].answer) {
+        score++;
+      }
     }
   });
 
   $('.key-container').click(function() {
     $(this).fadeOut( 500 );
     $('p', '#prompt').text(questionOne.question);
-    $('#choice-one').text(questionOne.answerOne[0]);
-    $('#choice-two').text(questionOne.answerTwo[0]);
-    $('#choice-three').text(questionOne.answerThree[0]);
+    $('#choice-one').html(questionOne.answer);
+    $('#choice-two').html(questionOne.wrongOne);
+    $('#choice-three').html(questionOne.wrongTwo);
     $('#quiz').slideDown( 1200 );
     $('#status').slideDown( 1200 );
   });
